bbbe76d1e9298f1128ef3113e3877c2b2ee1dc65
JiangYibai666 <yibaijiang95@gmail.com>
2025-11-10 15:46:14 +0800

    Assignment 2

--- ex1.py
+++ ex1.py
@@ -1,3 +1,14 @@
 def find(s, n):
-# write your implementation here
+    num_map = {}
+    for i, num in enumerate(s):
+        complement = n - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
     return None
+
+if __name__ == "__main__":
+    print(find([2,7,11,15], 9))
+    print(find([3,2,4], 6))
+    print(find([3,3], 6))
+    print(find([1,2,3,4], 8))
